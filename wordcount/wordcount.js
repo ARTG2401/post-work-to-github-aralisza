d56d92238a5074797de0cc781c29638652b36601
Emily Shi <shi.yue@husky.neu.edu>
2016-03-12 00:27:04 -0500

    oops

--- wordcount/wordcount.js
+++
@@ -1,7 +0,0 @@
-var input = document.getElementById('input').value;
-
-function countWords(str) {
-    return str.split(" ").length;
-}
-
-document.getElementById('words').value = countWords(input);
